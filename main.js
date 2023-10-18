314a5bb48b085e1f02dbed183f4bb9cb73094c08
Mafasyafa26 <60589488+Mafasyafa26@users.noreply.github.com>
2023-10-18 16:46:41 +0700

    laman

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var typed = new Typed(".text", {
     strings: ["QA / QC Engineer", "Manual / Automation Testing Engineer"],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1000,
+    backDelay: 100,
     loop: true
   });
